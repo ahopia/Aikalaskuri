e2b1ed9428607ecfea9c5658e0012b4c0924d8d8
Arto Hopia <arto.hopia@gmail.com>
2018-08-17 00:16:30 +0300

    Lisätty input string ReadLine

--- AikaEroLaskuri/Program.cs
+++ AikaEroLaskuri/Program.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// @author Arto Hopia
+/// version 17.8.2018
+/// Laskee aikaeron kahden päivämäärän välillä tulostaen sen muodossa pvm:dd:min
+/// </summary>
+
 namespace AikaEroLaskuri
 {
     class Program
@@ -11,21 +17,33 @@ namespace AikaEroLaskuri
         static void Main(string[] args)
         {
 
+            Console.WriteLine("Anna alku pvm muodossa vvvv-kk-pp hh:mm");   // tee try-parse oikeaan formaattiin
+            Console.Write("> ");
+            string alkuAika1 = Console.ReadLine();
 
-            string dateString2 = "2018-04-30 10:40";
-            string dateString3 = "2018-05-02 13:05";
+            Console.WriteLine("\r\nAnna loppu pvm muodossa vvvv-kk-pp hh:mm");
+            Console.Write("> ");
+            string loppuAika2 = Console.ReadLine();
+
+            //string dateString2 = "2018-04-30 10:40";
+            //string dateString3 = "2018-05-02 13:05";
+
+            string dateString2 = loppuAika2;
+            string dateString3 = alkuAika1;
 
             DateTime vaihdettu1 = MuutaPvm(dateString2);
             DateTime vaihdettu2 = MuutaPvm(dateString3);
 
             TimeSpan tulos = PvmEro(vaihdettu2, vaihdettu1);
 
-            Console.WriteLine(tulos);
+            Console.WriteLine(tulos + "\r\n");
 
             Console.WriteLine("Päiviä: \t" + tulos.Days);
-            Console.WriteLine("Tunteja: \t" + tulos.Hours);
-            Console.WriteLine("Minuutteja: \t" + tulos.Minutes);
+            Console.WriteLine("Tunteja: \t" + tulos.Hours");
+            Console.WriteLine("Minuutteja: \t" + tulos.Minutes + "\r\n");
 
+
+            Console.WriteLine(tulos.Days + " päivää " + tulos.Hours + " tuntia " + tulos.Minutes + " minuuttia");
             Console.ReadLine();
         }
 
