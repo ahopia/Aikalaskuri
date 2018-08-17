0920f06bd33089f0706cea3e3ccc356d8a629878
Arto Hopia <arto.hopia@gmail.com>
2018-08-17 23:15:36 +0300

    Lisätty pvm-luku funktio

--- AikaEroLaskuri/Program.cs
+++ AikaEroLaskuri/Program.cs
@@ -17,52 +17,73 @@ namespace AikaEroLaskuri
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Anna alku pvm muodossa vvvv-kk-pp hh:mm");   // tee try-parse oikeaan formaattiin
-            Console.Write("> ");
-            string alkuAika1 = Console.ReadLine();
+            Console.WriteLine("Anna alku pvm muodossa vvvv-kk-pp hh:mm");   
+            DateTime alkuAika = LueLuku();
 
             Console.WriteLine("\r\nAnna loppu pvm muodossa vvvv-kk-pp hh:mm");
-            Console.Write("> ");
-            string loppuAika2 = Console.ReadLine();
+            DateTime loppuAika = LueLuku();
 
-            //string dateString2 = "2018-04-30 10:40";
-            //string dateString3 = "2018-05-02 13:05";
+            TimeSpan tulos = PvmEro(loppuAika, alkuAika);
 
-            string dateString2 = loppuAika2;
-            string dateString3 = alkuAika1;
-
-            DateTime vaihdettu1 = MuutaPvm(dateString2);
-            DateTime vaihdettu2 = MuutaPvm(dateString3);
-
-            TimeSpan tulos = PvmEro(vaihdettu2, vaihdettu1);
-
-            Console.WriteLine(tulos + "\r\n");
+            Console.WriteLine("\r\nPäivämäärien ero on " + tulos + "\r\n");
 
             Console.WriteLine("Päiviä: \t" + tulos.Days);
-            Console.WriteLine("Tunteja: \t" + tulos.Hours");
+            Console.WriteLine("Tunteja: \t" + tulos.Hours);
             Console.WriteLine("Minuutteja: \t" + tulos.Minutes + "\r\n");
-
 
             Console.WriteLine(tulos.Days + " päivää " + tulos.Hours + " tuntia " + tulos.Minutes + " minuuttia");
             Console.ReadLine();
         }
 
+        private static DateTime LueLuku() // tätä voisi käyttää luvu lukemiseen ja formaatin tarkistamiseen
+        {
+            Boolean luuppi = true;
+            Console.Write("> ");
+            DateTime tulos = DateTime.MinValue;
+
+            while (luuppi)
+            {
+                string annettuPvm = Console.ReadLine();
+                Boolean onnistui = ParsePvm(annettuPvm, out tulos);
+                if (onnistui)
+                {
+                    luuppi = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\r\nLuku väärässä formaatissa, anna pvm uudelleen muodossa \r\n  vvvv-kk-pp hh:mm");
+                    Console.Write("> ");
+                }
+
+            }
+
+            return tulos;
+
+        }
+
+
+        public static Boolean ParsePvm(string pvm, out DateTime muuttuja)
+        {
+            return DateTime.TryParse(pvm, out muuttuja);
+        }
+
+
         public static DateTime MuutaPvm(string pvm)
         {
             DateTime muutettu = DateTime.Parse(pvm, System.Globalization.CultureInfo.InvariantCulture);
-
             return muutettu;
 
         }
 
 
         public static TimeSpan PvmEro(DateTime alku, DateTime loppu)
-
         {
             return (alku - loppu);
 
         }
 
-
     }
+
+
 }
